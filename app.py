9a3a5910c49545b21bf84d5590d9d699e9721a59
David Amini <aa224rw@student.lnu.se>
2024-10-27 18:54:51 +0100

    minor changes

--- app.py
+++
@@ -1,9 +0,0 @@
-import src.yatzy_round as yatzy_round
-
-def main() -> int:
-    yatzy_round.throw_dice()
-    return 0
-
-
-if __name__ == "__main__":
-    main()
--- src/classes.py
+++ src/classes.py
@@ -5,22 +5,21 @@ import os
 def clear_screen():
     os.system('cls' if os.name == 'nt' else 'clear')
 
-def only_nums(s: str):
+def only_nums(ans: str):
     choices = []
-    for i in s:
-        if i.isalnum():
-            choices.append(int(i))
+    for char in ans:
+        if char.isalnum():
+            choices.append(int(char))
     return choices
 
 class Dice:
-    def __init__(self, sides=6):
-        self.sides = sides
+    def __init__(self):
+        self.sides = 6
         self.value = None
         self.locked = False
 
     def roll(self):
-        if not self.locked:
-            self.value = random.randint(1, self.sides)
+        self.value = random.randint(1, self.sides)
         return self.value
 
     def lock(self):
@@ -29,40 +28,43 @@ class Dice:
     def unlock(self):
         self.locked = False
 
+    def get_value(self):
+        return self.value
+
 class DiceSet:
-    def __init__(self, num_dice=5, sides=6):
-        self.dice = [Dice(sides) for _ in range(num_dice)]
+    def __init__(self, num_dice):
+        self.dice = [Dice() for _ in range(num_dice)]
 
     def roll_all(self):
-        return [die.roll() for die in self.dice]
+        return [dice.roll() for dice in self.dice]
 
     def roll_unlocked(self):
-        return [die.roll() if not die.locked else die.value for die in self.dice]
+        return [dice.roll() if not dice.locked else dice.value for dice in self.dice]
 
-    def lock_dice(self, indices):
+    def lock_dice(self, indices: list):
         for index in indices:
             self.dice[index-1].lock()
 
-    def unlock_dice(self, indices):
+    def unlock_dice(self, indices: list):
         for index in indices:
             self.dice[index].unlock()
 
     def values(self):
-        return [die.value for die in self.dice]
+        return [dice.get_value for dice in self.dice]
 
     def reset(self):
-        for die in self.dice:
-            die.unlock()
-            die.value = None
+        for dice in self.dice:
+            dice.unlock()
+            dice.value = None
 
 
 
 while True:
     clear_screen()
-    dice_set = DiceSet(num_dice=5)
+    dice_set = DiceSet(5)
     print("First roll:", dice_set.roll_all())
     x = input("Do you want to re-roll: ")
-
+    result = []
     if x.lower() == "y":
         y = input("Which ones do you want to lock: (1,2,3,4,5): ")
         clear_screen()
